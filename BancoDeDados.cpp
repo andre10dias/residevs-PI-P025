3aaf20bffa3b3bdd403459b53e0b85984c5bc99e
andre10dias <andre10dias@ymail.com>
2023-10-26 18:01:07 -0300

    Banco de dados

---
+++ BancoDeDados.cpp
@@ -0,0 +1,103 @@
+#include "BancoDeDados.h"
+
+void BancoDeDados::salvarDados(vector<string> listaDados, string nomeArquivo) {
+    ofstream saida;
+
+    saida.open(nomeArquivo, ios_base::out);
+
+    if (saida.is_open())
+    {
+        for (string dado : listaDados)
+        {
+            saida << dado;
+        }
+        
+        saida << ";";
+        saida.close();
+    }
+    else
+    {
+        cout << "Falha ao abrir o arquivo.";
+    }
+}
+
+vector<string> BancoDeDados::recuperarDados(vector<string> listaDados, string nomeArquivo) {
+    ifstream entrada;
+
+    entrada.open(nomeArquivo, ios_base::in);
+
+    if (entrada.is_open())
+    {
+        string linha;
+        while (entrada.eof() == false)
+        {
+            getline(entrada, linha);
+            listaDados.push_back(linha);
+        }
+
+        entrada.close();
+    }
+    else
+    {
+        cout << "Falha ao abrir o arquivo.";
+    }
+
+    return listaDados;
+}
+
+vector<string> BancoDeDados::tratarDados(vector<string> listaDados) {
+    vector<string> listaCampos;
+    vector<string> listaLinhas;
+    string linha;
+    string campo;
+
+    for (string str : listaDados)
+    {
+        istringstream ss(str);
+
+        while (getline(ss, linha, ';')) {
+            listaLinhas.push_back(linha);
+        }
+    }
+    
+
+    for (string str : listaLinhas)
+    {
+        istringstream ss(str);
+
+        while (getline(ss, campo, ',')) {
+            listaCampos.push_back(campo);
+        }
+    }
+
+    return listaCampos;
+}
+
+vector<string> BancoDeDados::retornarLinha(vector<string> listaDados) {
+    vector<string> listaLinhas;
+    string linha;
+
+    for (string str : listaDados)
+    {
+        istringstream ss(str);
+
+        while (getline(ss, linha, ';')) {
+            listaLinhas.push_back(linha);
+        }
+    }
+
+    return listaLinhas;
+}
+
+vector<string> BancoDeDados::retornarCampos(string linha) {
+    vector<string> listaCampos;
+    string campo;
+
+    istringstream ss(linha);
+
+    while (getline(ss, campo, ',')) {
+        listaCampos.push_back(campo);
+    }
+
+    return listaCampos;
+}
---
+++ include/BancoDeDados.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class BancoDeDados
+{
+    private:
+        /* data */
+    public:
+        BancoDeDados(/* args */);
+        ~BancoDeDados();
+
+        static void salvarDados(vector<string> listaDados, string nomeArquivo);
+        static vector<string> recuperarDados(vector<string> listaDados, string nomeArquivo);
+        static vector<string> tratarDados(vector<string> listaDados);
+        static vector<string> retornarLinha(vector<string> listaDados);
+        static vector<string> retornarCampos(string linha);
+};

2877556a9470f83abdf0547a3ef9bfc16f068fb6
andre10dias <andre10dias@ymail.com>
2023-10-26 18:35:05 -0300

    Classe tweet

---
+++ Tweet.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Usuario.h"
+
+using namespace std;
+
+class Tweet
+{
+    private:
+        Usuario autor;
+        string conteudo;
+
+    public:
+        Tweet(/* args */);
+        ~Tweet();
+};
+
+Tweet::Tweet(/* args */)
+{
+}
+
+Tweet::~Tweet()
+{
+}
